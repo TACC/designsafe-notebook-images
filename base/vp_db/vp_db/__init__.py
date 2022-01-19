61154214501b93e77342521313996ff12d0c31ae
Joseph Vantassel <jvantassel@utexas.edu>
2022-01-19 16:26:17 -0600

    :fire: rename nbase to base and remove all other files

--- base/vp_db/vp_db/__init__.py
+++
@@ -1,7 +0,0 @@
-# Establishes connection to VP database from MySQL Workbench
-import pymysql
-def connect():
-    try:
-        return(pymysql.connect(user='dspublic', password='R3ad0nlY', host='129.114.52.174', port=3306, db='sjbrande_vpdb'))
-    except:
-        pass
